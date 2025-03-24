f83804b0e25f1209b866904ff00de8d08d14242c
force-push-main <10535@holbertonstudents.com>
2025-03-24 06:17:55 +0000

    added boilerplate code for _printf.c

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _printf - Entry point
+ *
+ * Description: prints to std output based on format
+ * @format: array of chars indicating format for args
+ * Return: number of chars printed excluding null terminator
+ */
+
+int _printf(const char *format, ...)
+{
+
+
+}
